2405cc50c247f5e25bce91a39b61c65554aff497
RichardInnocent <richjinnocent@gmail.com>
2022-10-23 16:54:26 +0100

    feat: Added Go CDK example

--- country-populations/cdk-examples/go/go.go
+++ country-populations/cdk-examples/go/go.go
@@ -8,7 +8,6 @@ import (
 	awslambdago "github.com/aws/aws-cdk-go/awscdklambdagoalpha/v2"
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
-	// "github.com/aws/jsii-runtime-go"
 )
 
 type GoStackProps struct {
